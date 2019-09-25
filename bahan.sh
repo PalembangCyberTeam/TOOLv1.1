674ea3e19b13d2bbda3ccfbb06821a2b7afbe88f
PalembangCyberTeam <55773159+PalembangCyberTeam@users.noreply.github.com>
2019-09-25 17:53:47 +0700

    Add files via upload

---
+++ bahan.sh
@@ -0,0 +1,10 @@
+#install bahan
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pkg install python
+pkg install toilet
+pkg install figlet
+pkg install curl
+pkg install wget
+pkg install bash
