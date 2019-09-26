a614aff8d4961089c5147c5a6118177b35b8eb4e
PalembangCyberTeam <55773159+PalembangCyberTeam@users.noreply.github.com>
2019-09-26 20:30:26 +0700

    Add files via upload

---
+++ nick.sh
@@ -0,0 +1,49 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+echo $me"╔════════╗"
+echo $me"║$pu Leader$me ║"
+echo $me"╚════════╝"
+echo $me"╔════════════╗"
+echo $me"║$pu ZX'Phoenix$me ║"
+echo $me"╚════════════╝"
+echo
+echo
+sleep 2
+echo $me"╔════════╗"
+echo $me"║$pu Member$me ║"
+echo $me"╚════════╝"
+echo $me"╔═════════════════════════════╗"
+echo $me"║$pu MiSetya ~ Chu404 ~ Reentz"
+echo $me"║$pu RC_Hunter ~ Pandora ~"
+echo $me"║$pu Mr.White ~ Mr.Agency ~"
+echo $me"║$pu Gatra ~ Mr./F4LL ~ w4rnetx"
+echo $me"║$pu Mr.s3lf ~ MR.ID20《X》"
+echo $me"║$pu Zona Nolep ~ Mr.Dongkrak"
+echo $me"║$pu Mr.Yazz ~ Mr.Sempak404"
+echo $me"║$pu Im/Panjul21"
+echo $me"╚═════════════════════════════╝"
+echo
+echo
+sleep 2
+echo $me"╔══════════════════════════════════════════════════════════════╗"
+echo $me"║$pu ┏━┓┏━┓╻  ┏━╸┏┳┓┏┓ ┏━┓┏┓╻┏━╸   ┏━╸╻ ╻┏┓ ┏━╸┏━┓   ╺┳╸┏━╸┏━┓┏┳┓$me ║"
+echo $me"║$pu ┣━┛┣━┫┃  ┣╸ ┃┃┃┣┻┓┣━┫┃┗┫┃╺┓   ┃  ┗┳┛┣┻┓┣╸ ┣┳┛    ┃ ┣╸ ┣━┫┃┃┃$me ║"
+echo $me"║$pu ╹  ╹ ╹┗━╸┗━╸╹ ╹┗━┛╹ ╹╹ ╹┗━┛   ┗━╸ ╹ ┗━┛┗━╸╹┗╸    ╹ ┗━╸╹ ╹╹ ╹$me ║"
+echo $me"╚══════════════════════════════════════════════════════════════╝"
+exit
